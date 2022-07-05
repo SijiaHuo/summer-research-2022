03ac56dbb7bfdd350cf0565ae405096ce52e783e
Javlon Nizomov <86540917+jnizomov@users.noreply.github.com>
2022-07-05 13:33:04 -0400

    Create predict.R

---
+++ predict.R
@@ -0,0 +1,15 @@
+pred <- dat %>%
+  mutate(p_hat = predict(fit, newdata = dat, type = "response"))
+
+# logit prediction
+
+pred <- pred %>%
+  mutate(logit = log(p_hat / (1 - p_hat))) %>%
+  gather(key = "predictors", value = "predictor.value", -logit)
+
+pred$p_hat[is.na(pred$p_hat)] <- 0
+
+ggplot(pred, aes(logit, predictor.value)) +
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") + 
+  facet_wrap(~predictors, scales = "free_y")
