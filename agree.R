dbebb2abb346c22ac2d04f6eda393d93ff6f3500
Javlon Nizomov <86540917+jnizomov@users.noreply.github.com>
2022-07-11 01:08:46 -0400

    Update agree.R

--- agree.R
+++ agree.R
@@ -1,8 +1,6 @@
 library(lubridate)
 library(tidyverse)
 
-#runs the same function from the analysis.R file, but slightly modified 
-
 pdat <- tab %>% #tab %>% 
   filter(result %in% c("positive", "negative") & date>=first_day) %>%
   group_by(testType, date) %>%
@@ -10,23 +8,19 @@ pdat <- tab %>% #tab %>%
             n = n(), patientId = patientId, result = result, .groups = "drop") %>%
   ungroup() %>%
   group_by(patientId) %>%
-  filter(length(unique(testType))!=1)
-
-  #filter(as.integer(n_distinct(testType)) != 1) #gets rid of patients who have data on the same test type (no comparisons possible)
+  filter(length(unique(testType))!=1) #gets rid of patients who have data on the same test type (no comparisons possible)
 
 filtered_pdat <- pdat %>% 
   arrange(date) %>%
   mutate(lagDate = lag(date), days_elapsed = as.numeric(date - lagDate)) %>%
-  mutate(p = k/n)
-
-filtered_pdat2 <- filtered_pdat %>%
+  mutate(p = k/n) %>%
   group_by(patientId) %>%
   filter(days_elapsed<=7) %>%
   filter(length(unique(testType))==2)
 
-sample <- unique(filtered_pdat2$patientId)[50:100]
+sample <- unique(filtered_pdat$patientId)[50:100]
 
-filtered_pdat2 %>%
+filtered_pdat %>%
   filter(patientId %in% sample) %>%
   ggplot(aes(date, p, shape=testType, color=result)) +
   geom_text(aes(label=substr(testType, 1, 1)), size=3.5) +
