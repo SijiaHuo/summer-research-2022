5d7c2a293dcf890f66cb7b7f96817460c52bf4e6
Javlon Nizomov <86540917+jnizomov@users.noreply.github.com>
2022-07-10 21:09:17 -0400

    Update agree.R

--- agree.R
+++ agree.R
@@ -1,19 +1,26 @@
 library(lubridate)
 library(tidyverse)
-  
-#filter for patient_ids with at least 1 unique test type in its history
-pg_tab <- tab %>% group_by(patientId) %>%
-  filter(as.integer(n_distinct(testType)) != 1,)
 
-#split df by patient_id and get first 100 df to sample
-pg_tab <- split(pg_tab, pg_tab$patientId)[1:100]
+#runs the same function from the analysis.R file, but slightly modified 
 
-for (df in pg_tab) {
-  #get differences between dates
-  df <- mutate(df, LagDate = lag(df$date), Diff = as.numeric(df$date - LagDate)) 
-  
-  #date difference greater than 4 days, then remove df
-  if (df$Diff[2] > 4) {
-    rm(df)
-  }
-}
+pdat <- tab %>% #tab %>% 
+  filter(date >= result %in% c("positive", "negative") & date>=first_day) %>%
+  group_by(testType, date) %>%
+  summarize(k = sum(result=="positive"),
+            n = n(), patientId = patientId, .groups = "drop") %>% 
+  select(patientId, testType, date, n, k) %>%
+  ungroup() %>%
+  group_by(patientId) %>%
+  filter(as.integer(n_distinct(testType)) != 1, ) #gets rid of patients who have data on the same test type (no comparisons possible)
+
+filtered_pdat <- pdat %>% 
+  arrange(date) %>%
+  mutate(lagDate = lag(date), days_elapsed = as.numeric(date - lagDate)) %>%
+  filter(days_elapsed <= 4) %>% # excludes tests done that aren't within 4 days of lag date
+  mutate(p = n/k)
+
+filtered_pdat %>% 
+  group_by(patientId) %>%
+  ggplot(aes(date, p, shape=testType, color=testType)) +
+  scale_shape_manual(values=c(3, 16, 17)) +
+  geom_point() 
